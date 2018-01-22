6bf3c2b3d841480d27e278c5b6e94e4e8cce6afa
Alejandro Lazo <alazo8807@gmail.com>
2018-01-22 16:46:55 +0000

    fixed start script2

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
 });
